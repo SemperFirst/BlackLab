228d16e52f464b521f8226e8f99c141cb7352dc0
SemperFirst <373052423@qq.com>
2024-12-26 17:43:54 +0800

    update

---
+++ scanner/tcp_syn_scanner_final/main.go
@@ -0,0 +1,25 @@
+package main 
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/urfave/cli"
+	"SafeDp/scanner/tcp_syn_scanner_final/cmd"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "port_scanner"
+	app.Auther = "SemperFi"
+	app.Usage = "A tcp/syn port scanner"
+	app.Version = "1.0.0"
+	app.Commands = []cli.Command{cmd.Scan}
+	app.Flags = append(app.Flags, cmd.Scan.Flags...)
+	err := app.Run(os.Args)
+	_ = err
+}
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
