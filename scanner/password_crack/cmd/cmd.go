2bf2452ba41159bbe166eac43d50e6e81a0b0eff
SemperFirst <373052423@qq.com>
2025-01-06 17:47:34 +0800

    update

--- scanner/password_crack/cmd/cmd.go
+++ scanner/password_crack/cmd/cmd.go
@@ -2,13 +2,45 @@ package cmd
 
 import (
 	"SafeDp/scanner/password_crack/util"
+
 	"github.com/urfave/cli"
 )
 
 var Scan = cli.Command{
-	Name: "Scan",
-	Usage: "Start to crack weak password",
+	Name:        "Scan",
+	Usage:       "Start to crack weak password",
 	Description: "Start to crack weak password",
-	Action: util.Scan,
+	Action:      util.Scan,
 	Flags: []cli.Flag{
+		boolFlag("debug, d", "debug mode"),
+		intFlag("timeout, t", 5, "timeout"),
+		intFlag("scan_num, n", 5000, "thread num"),
+		stringFlag("ip_list, i", "ip_list.txt", "ip_list"),
+		stringFlag("user_dict, u", "user.dic", "user dict"),
+		stringFlag("pass_dict, p", "pass.dic", "pass dict"),
+		stringFlag("outfile, o", "x_crack.txt", "scan result file"),
+	},
+}
+
+func boolFlag(name, usage string) cli.BoolFlag {
+	return cli.BoolFlag{
+		Name:  name,
+		Usage: usage,
+	}
+}
+
+func intFlag(name string, value int, usage string) cli.IntFlag {
+	return cli.IntFlag{
+		Name:  name,
+		Value: value,
+		Usage: usage,
+	}
+}
+
+func stringFlag(name, value, usage string) cli.StringFlag {
+	return cli.StringFlag{
+		Name:  name,
+		Value: value,
+		Usage: usage,
+	}
 }
