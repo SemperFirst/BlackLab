303e71af4e418c63832875458beaafe258d73f66
SemperFirst <373052423@qq.com>
2025-06-13 23:28:25 +0800

    update password_crack/models/cache.go

--- scanner/password_crack/models/cache.go
+++ scanner/password_crack/models/cache.go
@@ -41,3 +41,36 @@ func SaveResult(result ScanResult, err error){
 		vars.CasheService.Set(k, result, cache.NoExpiration)
 	}
 }
+
+func CacheStatus() (count int, items map[string]cache.Item) {
+	count = vars.CacheService.ItemCount()
+	items = vars.CacheService.Items()
+	return count, items
+}
+
+func ResultTotal(){
+	vars.ProgressBar.Finish()
+	logger.Log.Info(fmt.Sprintf("Finshed scan, total result: %v, used time: %v",vars.CacheService.ItemCount(), time.Since(vars.StartTime)))
+}
+
+func SaveResultToFile() error {
+	return vars.CacheService.SaveFile("password_crack.db")
+}
+
+func DumpToFile(filename string) (err error) {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	_, items := CacheStatus()
+	for _, v := range items {
+		result := v.Object.(ScanResult)
+		_, _ = file.WriteString(fmt.Sprintf("%v:%v|%v,%v:%v\n",
+			result.Service.Ip,
+			result.Service.Port,
+			result.Service.Protocol,
+			result.Service.Username,
+			result.Service.Password),
+		)
+	}
+}
