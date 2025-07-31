ddd82520bc76e5a3b483c8df781545db0ce6ad24
SemperFirst <373052423@qq.com>
2025-07-31 18:36:20 +0800

    update

--- sniffer/send_arp/main.go
+++ sniffer/send_arp/main.go
@@ -1,3 +1,86 @@
+package main
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+func main(){
+	var ipAddr string
+	var localAddr string
+	var ip net.IP
+	var localIp net.IP
+	var eth string
+
+	if len(os.Args) != 4 {
+		fmt.Printf("usage: %v eth localip targetIp\n", os.Args[0])
+		os.Exit(0)
+	}
+	eth = os.Args[1]
+	localAddr = os.Args[2]
+	ipAddr = os.Args[3]
+
+	ip = net.ParseIP(ipAddr)
+	localIp = net.ParseIP(localAddr)
+
+	iFace, err := net.InterfaceByName(eth)
+	if err != nil {
+		panic.(err)
+	}
+	err = send(iFace, localIp, ip)
+	log.Printf("send arp to %v, iface: %v, err: %v\n", ip,iFace, err)
+}
+
+func send(iFace *net.Interface, localIp, ip net.IP) error {
+	handle, err := pcap.OpenLive(iFace.Name, 1024, true, pcap.BlockForever)
+	if err != nil {
+		return err
+	}
+	defer handle.Close()
+
+	stop := make(chan struct{})
+	go readARP(handle, iFace, stop)
+	defer close(stop)
+	for {
+		if err := writeARP(handle, iFace, localIp, ip); err != nil {
+			log.Printf("error writing packets on %v:%v", iFace.Name, err)
+			return err
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func readARP(handle *pcap.Handle, iFace *net.Interface, stop chan struct{}){
+	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
+	in := src.Packets()
+	for {
+		var packet gopacket.Packet
+		select {
+		case<-stop:
+				return
+		case packet = <- in:
+			arpLayer := packet.Layer(layers.LayerTypeARP)
+			if arpLayer == nil {
+				continue
+			}
+			arp := arpLayer.(*layers.ARP)
+			log.Printf("arp.Operation: %v, srcIP %v, dstIp: %v, srcMac:%v, dstMac:%v\n",
+				arp.Operation,
+				net.IP(arp.SourceProtAddress),
+				net.IP(arp.DstProtAddress),
+				net.HardwareAddr(arp.SourceHwAddress),
+				net.HardwareAddr(arp.DstHwAddress),
+			)
+		}
+
+	}
+}
+
+
 func writeARP(handle *pcap.Handle, iFace *net.Interface, localIp, ip net.IP) error{
 	eth := layers.Ethernet{
 		SrcMAC:	   iFace.HardwareAddr,
@@ -11,7 +94,18 @@ func writeARP(handle *pcap.Handle, iFace *net.Interface, localIp, ip net.IP) err
 		ProtAddressSize: 4,
 		Operation:	   layers.ARPRequest,
 		SourceHwAddress: []byte(iFace.HardwareAddr),
-		SourceProtAddress []byte(localIp),
-		DstHwAddress: []byte{0,0,0,0,0,0}
+		SourceProtAddress: []byte(localIp),
+		DstHwAddress: []byte{0,0,0,0,0,0},
 	}
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths: true,
+		ComputeChecksums: true,
+	}
+	arp.DstProtAddress = ip
+	_ = gopacket.SerializeLayers(buf, opts, &eth, &arp)
+	if err := handle.WritePacketData(buf.Bytes()); err != nil {
+		return err
+	}
+	return nil
 }
